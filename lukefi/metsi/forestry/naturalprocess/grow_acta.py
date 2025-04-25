4e6bcf8b5321f3638b756d0fb6198236c8024cd9
Mikko Niemi <73464733+Mikko-Niemi@users.noreply.github.com>
2025-04-25 15:00:38 +0300

    Update grow_acta.py
    Update Pukkala growth module

--- lukefi/metsi/forestry/naturalprocess/grow_acta.py
+++ lukefi/metsi/forestry/naturalprocess/grow_acta.py
@@ -1,7 +1,5 @@
-from collections import defaultdict
 import math
-from statistics import median
-from lukefi.metsi.data.model import ForestStand, ReferenceTree, TreeSpecies, SiteType, SoilPeatlandCategory
+from lukefi.metsi.data.model import ForestStand, TreeSpecies, SiteType, SoilPeatlandCategory
 from lukefi.metsi.forestry.preprocessing.naslund import naslund_height
 
 
@@ -16,20 +14,21 @@ def Pukkala_diameter_growth_by_species(
     sitetype: SiteType,
     soilpeat: SoilPeatlandCategory
 ) -> float:
-# Parameters (Pukkala et al. 2021):                                           
+    
+    # Parameters (Pukkala et al. 2021):                                           
     D_param = { 'Intercept':[-7.1552, -12.7527, -8.6306], 'sqrt_d':[0.4415, 0.1693, 0.5097], 'd':[-0.0685, -0.0301, -0.0829], \
             'ln_G_1':[-0.2027, -0.1875, -0.3864], 'BAL_Total':[-0.1236, -0.0563, 0], 'BAL_Spruce':[0, -0.0870, 0], \
             'BAL_Spruce_Broadleaf':[0, 0, -0.0545], 'ln_TS':[1.1198, 1.9747, 1.3163], 'Peat':[-0.2425, 0, 0], 'd_Pendula_or_Aspen':[0, 0, 0.0253], \
             'Fertility':{'Herb-rich':[0.1438,0.2688,0.2566], 'Mesic':[0,0,0], 'Sub-xeric':[-0.1754,-0.2145,-0.2256], 'Xeric':[-0.5163,-0.6179,-0.3237]} }
-
-# re-codings
+    
+    # Re-codings
     if spe == TreeSpecies.PINE:
         spi = 0
     elif spe == TreeSpecies.SPRUCE:
         spi = 1
     else:
         spi = 2
-
+    
     if sitetype == (SiteType.VERY_RICH_SITE or SiteType.RICH_SITE):
         site = 'Herb-rich'
     elif sitetype == SiteType.DAMP_SITE:
@@ -38,12 +37,12 @@ def Pukkala_diameter_growth_by_species(
         site = 'Sub-xeric'
     else:
         site = 'Xeric'
-
+    
     if soilpeat != SoilPeatlandCategory.MINERAL_SOIL:
         peat = 1
     else:
         peat = 0
-
+    
     ln_D_increment = (D_param['Intercept'][spi]
              + D_param['sqrt_d'][spi] * math.sqrt(initialDiameter)
              + D_param['d'][spi] * initialDiameter
@@ -54,8 +53,50 @@ def Pukkala_diameter_growth_by_species(
              + D_param['ln_TS'][spi] * math.log(TS) 
              + D_param['Fertility'][site][spi] 
              + D_param['Peat'][spi] * peat
-             + D_param['d_Pendula_or_Aspen'][spi] * 0 * initialDiameter) # deciduous always assumed betula pubesc. (no information in forest data standard & small effect) 
+             + D_param['d_Pendula_or_Aspen'][spi] * 0 * initialDiameter)       # Deciduous trees assumed Betula Pubescens 
+    
     return math.exp(ln_D_increment)
+
+
+def Pukkala_survival_by_species(
+    spe: TreeSpecies,
+    initialDiameter: float,
+    BAL_Total: float,
+    BAL_Pine: float,
+    BAL_Spruce: float,
+    BAL_S_B: float,
+    soilpeat: SoilPeatlandCategory
+) -> float:
+    
+    # Parameters (Pukkala et al. 2021):                                           
+    S_param = { 'Intercept':[4.1505, 9.6649, 3.6655], 'sqrt_d':[3.1513, 1.0157, 1.0650], 'd':[-0.3575, -0.1577, -0.1509], \
+            'BAL_Total':[-0.4001, 0, 0], 'BAL_Pine':[0, 0, -0.0326], 'BAL_Spruce':[0, -0.3244, 0], 'BAL_Spruce_Broadleaf':[0, 0, -0.2768], \
+            'Peat':[-0.3813, -0.7366, -0.3884], 'Aspen':[0, 0, -0.0562], 'Birch':[0, 0, 1.0780] }
+    
+    # Re-codings
+    if spe == TreeSpecies.PINE:
+        spi = 0
+    elif spe == TreeSpecies.SPRUCE:
+        spi = 1
+    else:
+        spi = 2
+    
+    if soilpeat != SoilPeatlandCategory.MINERAL_SOIL:
+        peat = 1
+    else:
+        peat = 0
+    
+    f_survival = (S_param['Intercept'][spi]
+                  + S_param['sqrt_d'][spi] * math.sqrt(initialDiameter)
+                  + S_param['d'][spi] * initialDiameter
+                  + S_param['BAL_Total'][spi] * (BAL_Total/math.sqrt(initialDiameter+1))
+                  + S_param['BAL_Pine'][spi] * (BAL_Pine/math.sqrt(initialDiameter+1))
+                  + S_param['BAL_Spruce'][spi] * (BAL_Spruce/math.sqrt(initialDiameter+1))
+                  + S_param['BAL_Spruce_Broadleaf'][spi] * (BAL_S_B/math.sqrt(initialDiameter+1))
+                  + S_param['Peat'][spi] * peat
+                  + S_param['Birch'][spi] * 1)              # Deciduous trees assumed Betula spp.
+    
+    return 1 / ( 1 + math.exp(-f_survival) )
 
 
 def grow_diameter_and_height(
@@ -87,6 +128,8 @@ def grow_diameter_and_height(
     stems_predicted = stems
     gs = [t.stems_per_ha*math.pi*(0.01*0.5*d)**2 for t,d in zip(trees,ds)]
     G = sum(gs)
+    D_pines = [ds[tree] for tree,sp in enumerate(sps) if sp==TreeSpecies.PINE]
+    G_pines = [gs[tree] for tree,sp in enumerate(sps) if sp==TreeSpecies.PINE]
     D_spruces = [ds[tree] for tree,sp in enumerate(sps) if sp==TreeSpecies.SPRUCE]
     G_spruces = [gs[tree] for tree,sp in enumerate(sps) if sp==TreeSpecies.SPRUCE]
     D_spruces_broads = [ds[tree] for tree,sp in enumerate(sps) if sp != TreeSpecies.PINE]
@@ -97,6 +140,7 @@ def grow_diameter_and_height(
         if D_current is None:
             D_current = 0
         BAL = sum([gs[tree] for tree,diam in enumerate(ds) if diam > D_current])
+        BAL_pines = sum([G_pines[tree] for tree,diam in enumerate(D_pines) if diam > D_current])
         BAL_spruces = sum([G_spruces[tree] for tree,diam in enumerate(D_spruces) if diam > D_current])
         BAL_spruces_broads = sum([G_spruces_broads[tree] for tree,diam in enumerate(D_spruces_broads) if diam > D_current])
     
@@ -107,9 +151,10 @@ def grow_diameter_and_height(
                 height_0 = 0
             height_5 = naslund_height(ds_predicted[i], t.species)
             hs_predicted[i] += (height_5 - height_0)
+            stems_predicted[i] = stems[i] * Pukkala_survival_by_species(t.species, D_current, BAL, BAL_pines, BAL_spruces, BAL_spruces_broads, landclass)
         else:
             ### Mitä tehdä alle 1,3 metrin pituisille puille? !!!
             ds_predicted[i] = 1
             hs_predicted[i] = 1.3
-            
+    
     return ds_predicted, hs_predicted, stems_predicted
