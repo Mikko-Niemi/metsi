87151c3f6822ab630ce50fd532a48e0d0da0763f
Mikko Niemi <73464733+Mikko-Niemi@users.noreply.github.com>
2025-08-22 11:15:43 +0300

    Update control-aemes.py
    Testing to export felled trees

--- control-aemes.py
+++ control-aemes.py
@@ -48,6 +48,8 @@ control_structure = {
                 {sequence: [
                     cross_cut_standing_trees,
                     collect_standing_tree_properties,
+                    cross_cut_felled_trees,
+                    collect_felled_tree_properties,
                     calculate_npv,
                     calculate_biomass,
                     report_state
